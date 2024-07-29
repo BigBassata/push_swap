a510777a0dfa3fe59b1820f2641b41e19948ce23
Liam Cohen <liamcohen.pro@gmail.com>
2024-07-29 18:45:03 +0200

    debut, checkargs

---
+++ push_swap.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: licohen <licohen@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 16:29:06 by licohen           #+#    #+#             */
+/*   Updated: 2024/07/29 18:38:40 by licohen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP
+# define PUSH_SWAP
+
+# include "libft/libft.h"
+
+# include <unistd.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+
+
+//*** Commands ***
+void	ft_error(char *msg);
+void	ft_check_args(int argc, char **argv);
+void	ft_free(char **str);
+
+
+
+
+#endif
